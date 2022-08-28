9922b6f0417f77f2a0d33562257a402f832059ae
Yasuo Fujita <mettoboshi.1108@gmail.com>
2022-08-29 00:21:37 +0900

    書式を調整

--- src/Sample1.php
+++ src/Sample1.php
@@ -8,9 +8,10 @@ use myapp\Hoge;
 use myapp\Fuga;
 
 $builder = new DI\ContainerBuilder();
-$builder->addDefinitions([Hoge::class => function (ContainerInterface $c) {
-    return new Hoge($c->get(Fuga::class));
-},
+$builder->addDefinitions([
+    Hoge::class => function (ContainerInterface $c) {
+        return new Hoge($c->get(Fuga::class));
+    },
     Fuga::class => function (ContainerInterface $c) {
         return new Fuga();
     },
